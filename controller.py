3fedca09aa649e6099fb86be5333c637b718d72a
Takuo OGAWA <oretakuan@gmail.com>
2020-03-04 00:54:08 +0900

    一応バックテストを動かせる（結果があっているかは不明）

--- controller.py
+++ controller.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import calendar
 import time
 from notification import mylogger
+import backtest
+from strategy.models import testresult
 
 # config.iniファイルから設定情報を取得
 config_ini = configparser.ConfigParser()
@@ -18,6 +20,7 @@ log_path = config_ini.get("LOG", "path")
 
 client = bitmex.Bitmex(apiKey, apiSecret)
 logger = mylogger.BotLogger(log_path, line_token)
+testlogger = mylogger.BotLogger("./test.log", None)
 
 
 def cancel_all_orders(orders, interval):
@@ -32,44 +35,82 @@ def cancel_all_orders(orders, interval):
         print("注文が" + (interval * 5) + "秒以内に約定しなかったため、キャンセルしました。")
 
 
-def runBot(strategy, intervalSec=10, lot=1, period=60):
+MODE_BACK_TEST = "MODE_BACK_TEST"
+MODE_REAL_TRADE = "MODE_REAL_TRADE"
+
+
+def runBot(strategy=sanpei.Sanpei(), lot=1, period=60, mode=MODE_BACK_TEST):
+    t1 = time.time()
+
+    ohlc_list = None
+    result = None
+    is_backtest = mode != MODE_REAL_TRADE
+    if is_backtest:
+        ohlc_list = backtest.get_price_from_json()  # バックテストのみで使用
+        result = testresult.BackTestResult()
+
     l_ohlc = None
+    intervalSec = 0 if is_backtest else 10
+    idx = 0  # バックテストのみで使用
     while True:
-        ohlc = client.get_price_by_idx(period)  # 確定している最新の足を取得
+        # 確定している最新の足を取得
+        ohlc = ohlc_list[idx] if is_backtest else client.get_price_by_idx(
+            period)
+
         # 初回ループ時のみl_ohlcにohlcをセット
         if l_ohlc == None:
             ohlc.print_price()
             l_ohlc = ohlc
+            idx += 1
             continue
 
         # orderが一定時間約定しなかった場合、キャンセルする
-        orders = client.fetch_open_orders()
-        if len(orders) != 0:
-            cancel_all_orders(orders, intervalSec)
+        if not is_backtest:
+            orders = client.fetch_open_orders()
+            if len(orders) != 0:
+                cancel_all_orders(orders, intervalSec)
 
         if ohlc.close_time != l_ohlc.close_time:
             ohlc.print_price()
-            qty = client.get_position()
+            qty = result.qty if is_backtest else client.get_position()  # TODO: change lot
             if qty != 0:
                 signal = strategy.closeSignal(ohlc, l_ohlc)
                 if signal:
-                    client.close_position(qty)
                     msg = "[{time}] close position price:{price} amount:{lot}".format(
                         time=ohlc.close_time_str(), price=ohlc.close, lot=lot)
-                    logger.log_and_notify(msg)
+
+                    if is_backtest:
+                        result.close(exit_price=ohlc.close)
+                        testlogger.print_log(msg)
+
+                    else:
+                        client.close_position(qty)
+                        logger.log_and_notify(msg)
             else:
                 side = strategy.entrySignal(ohlc, l_ohlc)
                 if side != "":
-                    client.create_limit_order(side, ohlc.close, lot)
                     msg = "[{time}] entry:{side} price:{price} amount:{lot}".format(
                         time=ohlc.close_time_str(), side=side, price=ohlc.close, lot=lot)
-                    logger.log_and_notify(msg)
+                    if is_backtest:
+                        result.entry(side, lot, ohlc.close)
+                        testlogger.print_log(msg)
+                    else:
+                        client.create_limit_order(side, ohlc.close, lot)
+                        logger.log_and_notify(msg)
 
             l_ohlc = ohlc
+        idx += 1
+        if idx >= len(ohlc_list):
+            result.print_result("./result.txt")
+            break
 
-        time.sleep(intervalSec)
+        if not is_backtest:
+            time.sleep(intervalSec)
+    t2 = time.time()
+    print(f'{len(ohlc_list)}本のバックテストに{round(t2-t1)}秒かかりました')
 
 
+runBot()
 # runBot(sanpei.Sanpei(), intervalSec=10, lot=1, period=60)
 # print(client.handle(client.get_price_by_idx, 60))
 # print(client.handle(client.cancel_all_orders))
---
+++ strategy/models/testresult.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+TAKER_FEE = 0.00075
+MAKER_FEE = -0.00025
+
+
+class BackTestResult():
+    def __init__(self):
+        self.buy_count = 0
+        self.buy_winning = 0
+        self.buy_return = []
+        self.buy_profit = []
+        self.sell_count = 0
+        self.sell_winning = 0
+        self.sell_return = []
+        self.sell_profit = []
+        self.slippage = []
+        self.log = []
+
+        self.entry_price = None
+        self.side = ""
+        self.qty = 0
+
+    def entry(self, side, lot, entry_price):
+        self.entry_price = entry_price
+        self.qty = lot
+        self.side = side
+
+    def close(self, exit_price):
+        # 取引手数料
+        #entry_trade_cost = round(self.qty * TAKER_FEE)
+        #exit_trade_cost = round(self.qty * TAKER_FEE)
+        #trade_cost = entry_trade_cost + exit_trade_cost
+        # self.slippage.append(trade_cost)
+        trade_cost = 0
+
+        if self.side == "buy":
+            buy_profit = (exit_price - self.entry_price) * \
+                self.qty - trade_cost
+            self.buy_count += 1
+            self.buy_profit.append(buy_profit)
+            self.buy_return.append(
+                round(buy_profit / self.entry_price * 100, 4))
+            if buy_profit > 0:
+                self.buy_winning += 1
+
+        if self.side == "sell":
+            sell_profit = (self.entry_price - exit_price) * \
+                self.qty - trade_cost
+            self.sell_count += 1
+            self.sell_profit.append(sell_profit)
+            self.sell_return.append(
+                round(sell_profit / self.entry_price * 100, 4))
+            if sell_profit > 0:
+                self.sell_winning += 1
+
+        self.qty = 0
+        self.side = ""
+        self.entry_price = None
+
+    def print_result(self, file_path):
+        buy_gross_profit = np.sum(self.buy_profit)
+        sell_gross_profit = np.sum(self.sell_profit)
+
+        print("バックテストの結果")
+        print("--------------------------")
+        print("買いエントリの成績")
+        print("--------------------------")
+        print("トレード回数  :  {}回".format(self.buy_count))
+        print("勝率          :  {}％".format(
+            round(self.buy_winning / self.buy_count * 100, 1)))
+        print("平均リターン  :  {}％".format(
+            round(np.average(self.buy_return), 4)))
+        print("総損益        :  {}円".format(
+            np.sum(self.buy_profit)))
+
+        print("--------------------------")
+        print("売りエントリの成績")
+        print("--------------------------")
+        print("トレード回数  :  {}回".format(self.sell_count))
+        print("勝率          :  {}％".format(
+            round(self.sell_winning / self.sell_count * 100, 1)))
+        print("平均リターン  :  {}％".format(
+            round(np.average(self.sell_return), 4)))
+        print("総損益        :  {}円".format(
+            np.sum(self.sell_profit)))
+
+        print("--------------------------")
+        print("総合の成績")
+        print("--------------------------")
+        print("総損益        :  {}円".format(
+            np.sum(self.sell_profit) + np.sum(self.buy_profit)))
+        print("手数料合計    :  {}円".format(np.sum(self.slippage)))
+
+        # ログファイルの出力
+        file = open(file_path, 'wt', encoding='utf-8')
+        file.writelines(self.log)
